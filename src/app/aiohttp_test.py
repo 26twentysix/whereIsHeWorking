221a0bdd8eaff55a99107d7c876c59ba16e0eb94
lil angel <cvndyperov@gmail.com>
2022-05-09 22:11:16 +0500

    idk

--- src/app/aiohttp_test.py
+++ src/app/aiohttp_test.py
@@ -3,15 +3,6 @@ import aiohttp
 import requests
 import time
 
-base_url = 'https://api.vk.com/method/'
-access_token = 'b6e53625b6e53625b6e53625d1b6994f0bbb6e5b6e53625d49779210bced5b7ea7f455f'
-api_ver = '5.131'
-my_dict = dict()
-users_stack = asyncio.Queue()
-group_members_count = 0
-group_id = ''
-offset = 0
-
 
 def request_maker(method, params):
     return base_url + method + '?' + params + '&access_token=' + access_token + '&v=' + api_ver
@@ -23,6 +14,10 @@ def set_group_members_count():
         requests.get(request_maker('groups.getById', 'group_id=' + group_id + '&fields=name,members_count')).json()[
             "response"][0]["members_count"]
 
+async def get_company_name(company_group_id, session):
+    async with session.get(request_maker('groups.getById', 'group_id=' + str(company_group_id))) as response:
+        data = await response.json()
+        return data['response'][0]['name']
 
 async def get_user_info(session):
     while not users_stack.empty():
@@ -36,23 +31,31 @@ async def get_user_info(session):
             for i in range(len(data['response'])):
                 current_user = data['response'][i]
                 if 'career' in current_user:
-                    for comp in current_user['career']:
-                        if 'company' in comp:
-                            if comp['company'] in my_dict:
-                                my_dict[comp['company']] += 1
+                    for current_user_company in current_user['career']:
+                        if 'company' in current_user_company:
+                            if current_user_company['company'] in working_place_dict:
+                                working_place_dict[current_user_company['company']] += 1
                             else:
-                                my_dict[comp['company']] = 1
+                                working_place_dict[current_user_company['company']] = 1
+                        elif 'group_id' in current_user_company:
+                            company = await get_company_name(current_user_company['group_id'], session)
+                            if company in working_place_dict:
+                                working_place_dict[company] += 1
+                            else:
+                                working_place_dict[company] = 1
 
 
 async def get_group_members(session):
     global offset
     if offset < group_members_count:
         async with session.get(request_maker('groups.getMembers',
-                                             'group_id=' + group_id + '&count=1000&offset=' + str(offset))) as group_members:
+                                             'group_id=' + group_id + '&count=1000&offset=' + str(
+                                                 offset))) as group_members:
             data = await group_members.json()
             await users_stack.put(data['response']['items'])
         offset += 1000
         await get_group_members(session)
+        await get_user_info(session)
     else:
         await get_user_info(session)
 
@@ -70,9 +73,14 @@ def set_group_id():
     global group_id
     group_id = group_link[15:]
 
-def init_script():
+
+def init():
     set_group_id()
     set_group_members_count()
+    global offset, working_place_dict, users_stack
+    offset = 0
+    working_place_dict = dict()
+    users_stack = asyncio.Queue()
 
 
 async def main():
@@ -82,10 +90,13 @@ async def main():
         task2 = get_user_info(session)
         await asyncio.gather(task1, task2)
         print(time.time() - t0)
-        print(my_dict)
 
 
 if __name__ == '__main__':
-    init_script()
+    base_url = 'https://api.vk.com/method/'
+    access_token = 'b6e53625b6e53625b6e53625d1b6994f0bbb6e5b6e53625d49779210bced5b7ea7f455f'
+    api_ver = '5.131'
+    init()
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main())
+    print(len(working_place_dict))
--- src/app/core.py
+++ src/app/core.py
@@ -1,37 +1,10 @@
 import time
-
+import asyncio
 import requests
 
-base_url = 'https://api.vk.com/method/'
-access_token = '95967a0724e6cbccafdb90812fd7fdea824061b4ba144ebe4e099c91a1cec5203a833113baf6c63470c38'
-api_ver = '5.131'
+from collections import deque
 
 
-def request_maker(method, params):
-    return base_url + method + '?' + params + '&access_token=' + access_token + '&v=' + api_ver
-
-
-group = requests.get(request_maker('groups.getById', 'group_id=math_mech&fields=name,members_count'))
-count = group.json()["response"][0]["members_count"]
-offset = 0
-my_dict = {}
-while offset < count:
-    time.sleep(0.3)
-    group_members = requests.get(request_maker('groups.getMembers',
-                                               'group_id=math_mech&count=1000&offset=' + str(offset)))
-    users_with_info = requests.get(request_maker('users.get',
-                                  'user_ids=' +
-                                  str(group_members.json()['response']['items'])[1:-1].replace(' ', '') +
-                                  '&fields=career'))
-    for i in range(len(users_with_info.json()['response'])):
-        current_user = users_with_info.json()["response"][i]
-        if 'career' in users_with_info.json()["response"][i]:
-            comp_list = users_with_info.json()["response"][i]['career']
-            for comp in comp_list:
-                if 'company' in comp:
-                    if comp['company'] in my_dict:
-                        my_dict[comp['company']] += 1
-                    else:
-                        my_dict[comp['company']] = 1
-    offset += 100
-print(my_dict)
+def check_valid_group_link(group_link: str):
+    return group_link[:15] == 'https://vk.com/' and group_link
+print(check_valid_group_link('htttps://vk.com/GROUP_ID'))
